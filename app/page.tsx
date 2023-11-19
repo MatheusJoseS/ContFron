7fe1e3157db5759c73c4e4550cd4a7c3a4a97dd6
unknown <matheusmorato957@gmail.com>
2023-11-19 07:26:49 -0300

    ... - 19/11

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,7 @@ interface TokenResponse {
 export default function Login() {
   const [ecadastro, setecadastro] = useState(false)
   const [loading, setLoading] = React.useState<boolean>(false)
-  const [goo, setGoo] = React.useState({title: '', body:''})
-
+  const [goo, setGoo] = React.useState({ title: '', body: '' })
   const [from, setFrom] = React.useState<{ email_user: string; nome_user: string; senha_user: string; senha_user_comfir: string; }>({ email_user: '', nome_user: '', senha_user: '', senha_user_comfir: '', })
   const [cor, setCor] = React.useState("#1E3A8A")
   const [erro, setErro] = React.useState('')
@@ -25,18 +24,18 @@ export default function Login() {
   const session = await getServerSession(authOpitions)
   const { fcmToken, notificationPermissionSattus } = useFcmToken();
   fcmToken && console.log('FCM token:', fcmToken);
- useEffect (() =>{
-  if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-    const messaging = getMessaging(firebaseApp);
-    const unsubscribe = onMessage(messaging, (payload)=>{
-      console.log('Foreground puch notification received:' ,payload);
-      setGoo({'title': payload.notification?.title || '', 'body': payload.notification?.body})
-    });
-    return () => {
-      unsubscribe();
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      const messaging = getMessaging(firebaseApp);
+      const unsubscribe = onMessage(messaging, (payload) => {
+        console.log('Foreground puch notification received:', payload);
+        setGoo({ 'title': payload.notification?.title || '', 'body': payload.notification?.body })
+      });
+      return () => {
+        unsubscribe();
+      }
     }
-  }
- })
+  })
   const mudanca = () => {
     limpar();
     cadastro();
@@ -154,24 +153,23 @@ export default function Login() {
         <h1 tabIndex={19} style={{ color: "#3D50B6" }} className='text-4xl underline text-center mt-6'><a href="/sobre"><strong> Sobre</strong></a></h1>
       </div>
 
-
-      <div id="toast-default" className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
-        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z"/>
-        </svg>
-        <span className="sr-only">Fire icon</span>
-    </div>
-    <div className="ms-3 text-sm font-normal">Set yourself free.</div>
-    <button type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
-        <span className="sr-only">Close</span>
-        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
-</div>
-
-
+      {goo.title &&
+        <div id="toast-default" className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
+            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z" />
+            </svg>
+            <span className="sr-only">Fire icon</span>
+          </div>
+          <div className="ms-3 text-sm font-normal">{goo.body}</div>
+          <button onClick={() => setGoo({title:'',body:''})} type="button" className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+            </svg>
+          </button>
+        </div>
+      }
     </main>
   )
 }
--- public/firebase-massaging-sw.js
+++ public/firebase-massaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDoTRW7E48ru-AuukI2dwfbcu9jt2wM_0g",
-    authDomain: "historinhas-18386.firebaseapp.com",
-    projectId: "historinhas-18386",
-    storageBucket: "historinhas-18386.appspot.com",
-    messagingSenderId: "377869314621",
-    appId: "1:377869314621:web:58ffe37b10a901514d9854",
-    measurementId: "G-G9REXF7HZK"
+  apiKey: "AIzaSyDoTRW7E48ru-AuukI2dwfbcu9jt2wM_0g",
+  authDomain: "historinhas-18386.firebaseapp.com",
+  projectId: "historinhas-18386",
+  storageBucket: "historinhas-18386.appspot.com",
+  messagingSenderId: "377869314621",
+  appId: "1:377869314621:web:58ffe37b10a901514d9854",
+  measurementId: "G-G9REXF7HZK"
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -26,12 +26,11 @@ messaging.onBackgroundMessage(function(payload) {
 */
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    body: Background.Message.body
   };
-
+  window.alert(payload)
   self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
